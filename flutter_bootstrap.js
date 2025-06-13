a23e9631de3ccd973105358a915340b6275fcfa4
Your Name <your.email@example.com>
2025-06-13 20:48:45 +0500

    Fix blank screen issue - correct base href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "17699876"
+    serviceWorkerVersion: "664360621"
   }
 });
